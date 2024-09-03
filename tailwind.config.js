fb80ab3a1e08e75f06f3c7740fbee2bf48544e4e
Kristal Pithva <>
2024-09-03 19:12:24 +0530

    Add Animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        rotate: {
+          "0%": { transform: "rotate(0deg)" },
+          "100%": { transform: "rotate(360deg)" },
+        },
+        wave: {
+          "0%, 100%": { transform: "rotate(0deg)" },
+          "50%": { transform: "rotate(40deg)" },
+        },
+      },
+      animation: {
+        rotate: "rotate 3s linear infinite",
+        wave: "wave 1s ease-in-out infinite",
+      },
+    },
   },
   plugins: [],
 };
